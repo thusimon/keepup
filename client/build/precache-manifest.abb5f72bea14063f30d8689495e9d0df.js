c30d93099ed324b10932656ab79612c77bfc5076
Lu.Xu <thusimon@gmail.com>
2020-08-02 16:21:07 -0700

    deploy 3

---
+++ client/build/precache-manifest.abb5f72bea14063f30d8689495e9d0df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80eaca9a83962885a7fc6b2b7cddc085",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b8b4d05024a19510886",
+    "url": "/static/css/main.c0710856.chunk.css"
+  },
+  {
+    "revision": "82fec0d7cea0d9464edc",
+    "url": "/static/js/2.ef1befab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ef1befab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8b4d05024a19510886",
+    "url": "/static/js/main.ef6e3e0b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
